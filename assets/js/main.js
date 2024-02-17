992334e0a43833309469aa524218104ea3e36c59
illancapan <igorllancapan@gmail.com>
2024-02-17 00:37:36 -0300

    Finalizado falto modificar los ultimo 12 meses

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,126 @@
-document.getElementById('search-btn').addEventListener('click', function() {
-    convert();
-});
+// arreglo nombre monedas
+const nombresMonedas = {
+  dolar: "Dólar",
+  dolar_intercambio: "Dólar intercambio",
+  euro: "Euro",
+  bitcoin: "Bitcoin",
+  uf: "UF",
+  utm: "UTM",
+  ivp: "IVP",
+  ipc: "IPC",
+  imacec: "IMACEC",
+  libra_cobre: "Libra cobre",
+  tasa_desempleo: "Tasa desempleo",
+  tpm: "TPM",
+};
 
-async function convert() {
-    try {
-        const clpAmount = document.getElementById('clp').value;
-        const coin = document.getElementById('coin').value;
+async function convertir() {
+  const selector = document.querySelector("#moneda");
+  const monedaSeleccionada = selector.value;
 
-        const response = await fetch(`https://mindicador.cl/api/${coin}`);
-        const data = await response.json();
+  if (monedaSeleccionada === "Seleccione moneda") {
+    alert("Debes seleccionar una moneda");
+    return;
+  }
 
-        const exchangeRate = data.serie[0].valor;
+  try {
+    const res = await fetch(`https://mindicador.cl/api/${monedaSeleccionada}`);
+    const data = await res.json();
 
-        const result = clpAmount * exchangeRate;
+    // Obtengo cifras tipo number
+    const pesosCLP = Number(document.querySelector("#clp").value);
+    const valorMoneda = Number(data.serie[0].valor);
 
-        document.getElementById('result').innerHTML = `El monto en ${coin.toUpperCase()} es: ${result.toLocaleString()} ${coin.toUpperCase()}`;
+    const conversion = (pesosCLP * valorMoneda).toLocaleString("es-CL", {
+      style: "currency",
+      currency: "CLP",
+    });
 
-        const ctx = document.getElementById('result-chart').getContext('2d');
+    // utilizo arreglo nombre monedas (nombresMonedas)
+    const nombreMonedaMostrar =
+      nombresMonedas[monedaSeleccionada] || monedaSeleccionada;
 
-        if (window.myChart instanceof Chart) {
-            window.myChart.destroy();
-        }
+    const resultado = document.querySelector("#resultado");
+    resultado.innerHTML = `Resultado: ${nombreMonedaMostrar.toUpperCase()} ${conversion}`;
 
-        const dataForChart = {
-            labels: ['CLP', coin.toUpperCase()],
-            datasets: [{
-                label: 'Monto convertido',
-                data: [
-                    { x: 'CLP', y: parseFloat(clpAmount) },
-                    { x: coin.toUpperCase(), y: parseFloat(result.toFixed(2)) }
-                ],
-                fill: false,
-                borderColor: 'rgba(255, 99, 132, 1)',
-                borderWidth: 2
-            }]
-        };
-
-        window.myChart = new Chart(ctx, {
-            type: 'line',
-            data: dataForChart,
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    } catch (error) {
-        console.error('Hubo un problema al procesar la solicitud:', error);
-        document.getElementById('result').innerHTML = 'Hubo un problema al procesar la solicitud.';
-    }
+    graficarMoneda(monedaSeleccionada);
+  } catch (error) {
+    alert(error.message);
+  }
 }
 
+let miGrafico;
 
+async function graficarMoneda(monedaSeleccionada) {
+  try {
+    const res = await fetch(`https://mindicador.cl/api/${monedaSeleccionada}`);
+    const data = await res.json();
+
+    const valoresX = [];
+    const valoresY = [];
+
+    const fechaActual = new Date();
+
+    for (let i = data.serie.length - 1; i >= 0; i--) {
+      const fechaDato = new Date(data.serie[i].fecha);
+
+      const fechaHaceUnAno = new Date(fechaActual);
+      fechaHaceUnAno.setFullYear(fechaActual.getFullYear() - 1);
+
+      if (fechaDato >= fechaHaceUnAno && fechaDato <= fechaActual) {
+        valoresX.push(data.serie[i].fecha.substring(0, 10));
+        valoresY.push(data.serie[i].valor);
+      }
+    }
+
+    const plugin = {
+      id: "fondoCanvas",
+      beforeDraw: (grafico, args, opciones) => {
+        const { ctx } = grafico;
+        ctx.save();
+        ctx.globalCompositeOperation = "destination-over";
+        ctx.fillStyle = opciones.color || "#99ffff";
+        ctx.fillRect(0, 0, grafico.width, grafico.height);
+        ctx.restore();
+      },
+    };
+
+    if (miGrafico) {
+      miGrafico.destroy();
+    }
+
+    const nombreMonedaMostrar =
+      nombresMonedas[monedaSeleccionada] || monedaSeleccionada;
+
+    miGrafico = new Chart("graficoResultado", {
+      type: "line",
+      data: {
+        labels: valoresX,
+        datasets: [
+          {
+            label: "Valores de " + nombreMonedaMostrar.toUpperCase(),
+            backgroundColor: "darkcyan",
+            borderColor: "darkcyan",
+            data: valoresY,
+            id: nombreMonedaMostrar,
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          fondoCanvas: {
+            color: "white",
+          },
+        },
+      },
+      plugins: [plugin],
+    });
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const btnBuscar = document.getElementById("btnBuscar");
+  btnBuscar.addEventListener("click", convertir);
+});
